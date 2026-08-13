03c191bb3abad32710ef90fe0e669977a4be47c0
karantrahal03-cmd <karantrahal03@gmail.com>
2026-08-13 13:50:02 +0530

    Add anti-cache headers and script versioning

--- server.js
+++ server.js
@@ -98,6 +98,14 @@ const parseTimes = (timeStr) => {
 
 
 
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.setHeader('Surrogate-Control', 'no-store');
+  next();
+});
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
